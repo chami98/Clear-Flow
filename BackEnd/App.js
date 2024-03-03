9e7aecacb83e2999960f92feba630deb119feeb1
chami98 <chamikara98@gmail.com>
2024-03-04 03:19:37 +0530

    enable cors

--- BackEnd/App.js
+++ BackEnd/App.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const admin = require('firebase-admin');
-
 const serviceAccount = require('./serviceAccountKey');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
