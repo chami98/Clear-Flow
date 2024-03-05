9dd5d12688b3238912a7cc8cbc442ede4e9dedc1
chami98 <chamikara98@gmail.com>
2024-03-06 02:32:00 +0530

    removed routing for github pages

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,20 +4,14 @@ import ComboBox from './ComboBox';
 import Dashboard from './Dashboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import SignInSide from './Pages/SignInSide';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-          <Route exact path="/dashboard" element={<Dashboard titlePlace="Publication" />} />
-          <Route exact path="/login" element={<SignInSide />} />
-        </Routes>
-        <ToastContainer />
-      </Router>
+      <Dashboard titlePlace="Publication" />
+      {/* <SignInSide /> */}
+      <ToastContainer />
     </>
   );
 }
