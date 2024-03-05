3132059fa69a76ca3e8bebc07aab237fd94cecf2
chami98 <chamikara98@gmail.com>
2024-03-06 02:24:14 +0530

    Add login and roueting

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,12 +4,20 @@ import ComboBox from './ComboBox';
 import Dashboard from './Dashboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import SignInSide from './Pages/SignInSide';
 
 function App() {
   return (
     <>
-      <Dashboard titlePlace="Publication" />
-      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route exact path="/dashboard" element={<Dashboard titlePlace="Publication" />} />
+          <Route exact path="/login" element={<SignInSide />} />
+        </Routes>
+        <ToastContainer />
+      </Router>
     </>
   );
 }
