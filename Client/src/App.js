00aa3ddf6d8a1e587788e537990928507fdfb257
chami98 <chamikara98@gmail.com>
2024-03-13 23:12:31 +0530

    Add routing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import ComboBox from './ComboBox';
@@ -9,17 +9,32 @@ import 'react-toastify/dist/ReactToastify.css';
 import SignInSide from './Pages/SignInSide';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Your authentication logic here
+    // For demonstration purposes, I'm using a simple local storage check
+    const isAuthenticated = true;
+    setAuthenticated(isAuthenticated);
+  }, []);
+
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route path="/signin" element={<SignInSide />} />
+          <Route
+            exact
+            path="/"
+            element={authenticated ? <Dashboard titlePlace="Publication" /> : <Navigate to="/signin" />}
+          />
+          <Route
+            path="/signin"
+            element={!authenticated ? <SignInSide /> : <Navigate to="/" />}
+          />
         </Routes>
         <ToastContainer />
       </Router>
     </>
-
   );
 }
 
