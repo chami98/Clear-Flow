149ad00b74a84e39fd1f8249c72a13a72df362ee
chami98 <chamikara98@gmail.com>
2024-03-18 20:40:29 +0530

    Remove drawer

--- Client/src/Dashboard.js
+++ Client/src/Dashboard.js
@@ -129,18 +129,6 @@ export default function Dashboard({ titlePlace }) {
                             pr: '24px', // keep right padding when drawer closed
                         }}
                     >
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={toggleDrawer}
-                            sx={{
-                                marginRight: '36px',
-                                ...(open && { display: 'none' }),
-                            }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
                         <Typography
                             component="h1"
                             variant="h6"
@@ -157,26 +145,6 @@ export default function Dashboard({ titlePlace }) {
                         </IconButton>
                     </Toolbar>
                 </AppBar>
-                <Drawer variant="permanent" open={open}>
-                    <Toolbar
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'flex-end',
-                            px: [1],
-                        }}
-                    >
-                        <IconButton onClick={toggleDrawer}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </Toolbar>
-                    <Divider />
-                    <List component="nav">
-                        {mainListItems}
-                        <Divider sx={{ my: 1 }} />
-                        {secondaryListItems}
-                    </List>
-                </Drawer>
                 <Box
                     component="main"
                     sx={{
